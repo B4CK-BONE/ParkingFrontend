52023a53a3335bec00a0ecddba69318ba5674e73
1223v <1223v@naver.com>
2023-05-24 04:04:51 +0000

    fix : auth function fixed

--- src/_actions/user_action.js
+++ src/_actions/user_action.js
@@ -1,26 +1,27 @@
-import Axios from "axios";
-import { AUTH_USER } from "./types";
+import Axios from 'axios';
+import { AUTH_USER } from './types';
 
 export function auth() {
-  var cookies = document.cookie; // 쿠키 문자열 가져오기
-  var cookieArray = cookies.split(";"); // 쿠키 문자열을 세미콜론으로 분할하여 배열 생성
+    let body = {};
+    if (document.cookie !== '') {
+        var cookies = document.cookie; // 쿠키 문자열 가져오기
+        var cookieArray = cookies.split(';'); // 쿠키 문자열을 세미콜론으로 분할하여 배열 생성
 
-  // 쿠키 이름과 값의 구분자 위치 찾기
-  var separatorIndex = cookieArray.indexOf("=");
+        // // 쿠키 이름과 값의 구분자 위치 찾기
+        var separatorIndex = cookieArray.indexOf('=');
 
-  var cookieValue = cookieArray.substring(separatorIndex + 1); // 쿠키 값 추출
-  var cookierealValue = cookieValue.trim(";");
-  const params = { id: cookierealValue };
-  const body = JSON.stringify(params);
+        var cookieValue = cookieArray.substring(separatorIndex + 1); // 쿠키 값 추출
+        var cookierealValue = cookieValue.trim(";");
+        let params = { id: cookierealValue };
+        body = JSON.stringify(params);
 
-  const request = Axios.post(
-    "https://backbone-ufribf.run.goorm.site/api/user/auth",
-    body,
-    { withCredentials: true }
-  ).then((response) => response.data);
-
-  return {
-    type: AUTH_USER,
-    payload: request,
-  };
+        
+    }
+    const request = Axios.post('https://backbone-ufribf.run.goorm.site/api/user/auth', body, {
+            withCredentials: true,
+        }).then((response) => response.data);
+    return {
+        type: AUTH_USER,
+        payload: request,
+    };
 }
