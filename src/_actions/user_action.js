22d9b8ae9635b03a5be30d678d57de8c741fae33
1223v <1223v@naver.com>
2023-05-29 13:02:55 +0000

    feat: auth function add

--- src/_actions/user_action.js
+++ src/_actions/user_action.js
@@ -2,23 +2,22 @@ import Axios from 'axios';
 import { AUTH_USER } from './types';
 
 export function auth() {
-    let body = {};
+    let body = {"id" : ''};
     if (document.cookie !== '') {
         var cookies = document.cookie; // 쿠키 문자열 가져오기
         var cookieArray = cookies.split(';'); // 쿠키 문자열을 세미콜론으로 분할하여 배열 생성
-
+		var str = cookieArray.join('');
         // // 쿠키 이름과 값의 구분자 위치 찾기
-        var separatorIndex = cookieArray.indexOf('=');
+        var cookieValue = str.split('=')[1];
 
-        var cookieValue = cookieArray.substring(separatorIndex + 1); // 쿠키 값 추출
-        var cookierealValue = cookieValue.trim(";");
-        let params = { id: cookierealValue };
+        
+        let params = { "id" : cookieValue };
         body = JSON.stringify(params);
-
+		console.log("test",body);
         
         
     }
-    const request = Axios.post('https://backbone-ufribf.run.goorm.site/api/user/auth', body, {
+    const request = Axios.post('https://backbone-ufribf.run.goorm.site/user/', body, {
             withCredentials: true,
         }).then((response) => response.data);
     return {
